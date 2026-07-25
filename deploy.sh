c098ede17b241f83ee1061a12bb85d473cf296f8
gaganDevops <118088965+gaganDevops@users.noreply.github.com>
2026-07-25 12:30:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,26 @@ case "$APPLICATION" in
 
 Retail)
 
-    HOST="15.206.243.220"
-    USER="ec2-user"
     TOMCAT_HOME="/opt/tomcat"
 
+    echo "======================================"
+    echo "Application : Retail"
+    echo "Deploying Locally"
+    echo "======================================"
+
+    echo "Removing old deployment..."
+    rm -rf ${TOMCAT_HOME}/webapps/hello-webapp
+    rm -f ${TOMCAT_HOME}/webapps/${WAR_NAME}
+
+    echo "Copying WAR..."
+    cp ${WAR_FILE} ${TOMCAT_HOME}/webapps/
+
+    echo "Restarting Tomcat..."
+    ${TOMCAT_HOME}/bin/shutdown.sh || true
+    sleep 5
+    ${TOMCAT_HOME}/bin/startup.sh
+
+    echo "Deployment Successful."
     ;;
 
 Corporate)
@@ -23,37 +39,37 @@ Corporate)
     USER="ec2-user"
     TOMCAT_HOME="/opt/tomcat/apache-tomcat-10.1.57"
 
+    echo "======================================"
+    echo "Application : Corporate"
+    echo "Host        : ${HOST}"
+    echo "======================================"
+
+    echo "Checking SSH Connection..."
+    ssh ${USER}@${HOST} "hostname"
+
+    echo "Removing old deployment..."
+    ssh ${USER}@${HOST} << EOF
+rm -rf ${TOMCAT_HOME}/webapps/hello-webapp
+rm -f ${TOMCAT_HOME}/webapps/${WAR_NAME}
+EOF
+
+    echo "Copying WAR..."
+    scp ${WAR_FILE} ${USER}@${HOST}:${TOMCAT_HOME}/webapps/
+
+    echo "Restarting Tomcat..."
+    ssh ${USER}@${HOST} << EOF
+${TOMCAT_HOME}/bin/shutdown.sh || true
+sleep 5
+${TOMCAT_HOME}/bin/startup.sh
+EOF
+
+    echo "Deployment Successful."
     ;;
 
 *)
 
     echo "Invalid Application"
     exit 1
-
     ;;
 
 esac
-
-echo "======================================"
-echo "Application : $APPLICATION"
-echo "Host        : $HOST"
-echo "======================================"
-
-echo "Checking SSH Connection..."
-
-ssh ${USER}@${HOST} "hostname"
-
-echo "Removing old deployment..."
-
-ssh ${USER}@${HOST} << EOF
-
-rm -rf ${TOMCAT_HOME}/webapps/hello-webapp
-rm -f ${TOMCAT_HOME}/webapps/${WAR_NAME}
-
-EOF
-
-echo "Copying WAR..."
-
-scp ${WAR_FILE} ${USER}@${HOST}:${TOMCAT_HOME}/webapps/
-
-echo "Deployment Successful."
